9cc59957d179108d1dd88dd10d57bb34cb748b5e
Lukas Bollinger <lukas.bollinger21@gmail.com>
2019-05-16 12:47:16 +0200

    Setup validation and ssosc

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  has_many :cocktails, trough: :doses, dependent: :restrict
+  has_many :cocktails, through: :doses
 
   validates :name, presence: true, uniqueness: true
 end
